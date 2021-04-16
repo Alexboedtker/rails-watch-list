d84dc2ec509ae6cd2c79d1f50a67f18383b32c9c
Alexandra Boedtker <alexboedtker@gmail.com>
2021-04-16 14:57:22 +0200

    card banner working

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,13 +13,17 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.create(list_params)
-    redirect_to list_path(@list)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
